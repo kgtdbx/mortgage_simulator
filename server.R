e80175c2c9b1e79f444a1ecf782f1b8e944491a3
Justin Brantley <Justin.Brantley@nordstrom.com>
2016-03-08 13:19:58 -0800

    Load data; Initialize ZillowR API interface

--- server.R
+++ server.R
@@ -1,14 +1,17 @@
 
 library(shiny)
+library(ZillowR)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
     output$distPlot <- renderPlot({
+set_zillow_web_service_id(readLines('zws_id.txt', warn = FALSE))
 
         # generate bins based on input$bins from ui.R
         x    <- faithful[, 2]
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
+load('data/future_rates.RData')
 
         # draw the histogram with the specified number of bins
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
