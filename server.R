9e3e078da08755ee0a8027a087a80e7dd15c4530
Justin Brantley <Justin.Brantley@nordstrom.com>
2016-03-08 15:10:12 -0800

    Render (uninformative) plot using inputs

--- server.R
+++ server.R
@@ -2,20 +2,15 @@
 library(shiny)
 library(ZillowR)
 
+set_zillow_web_service_id(readLines('zws_id.txt', warn = FALSE))
+
+load('data/future_rates.RData')
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
-set_zillow_web_service_id(readLines('zws_id.txt', warn = FALSE))
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-load('data/future_rates.RData')
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    output$mortgagePlot <- renderPlot({
+        plot(rnorm(input$price))
     })
 
 })
